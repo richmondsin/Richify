8876556042dda5274b740edccc28b56f44f4355b
Richmond Sin <richmondsin.rs@gmail.com>
2023-06-11 18:05:15 +0800

    initial

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -37,7 +37,7 @@ const AuthModal = () => {
             <Auth 
             theme="dark"
             magicLink
-            providers={["github", "google"]} //add login platforms
+            providers={[]} //add login platforms
             supabaseClient={supabaseClient}
             appearance={{
                 theme: ThemeSupa,
